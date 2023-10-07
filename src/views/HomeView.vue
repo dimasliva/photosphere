35d6efcc58f20d3c5623894881c2ab56acc5d8ed
dimasliva <janzerikYT@yandex.ru>
2023-10-07 16:19:03 +0500

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -160,7 +160,7 @@ export default defineComponent ({
 
 <template>
   <div class="container">
-    <span v-if="err.length !== 0" class="error">{{ err }}</span>
+    <span v-if="err" class="error">{{ err }}</span>
     <div class="form">
       <div class="block">
         <span>Имя</span>
